20e08eec391762ff0e71aa06af43cce1c5f23327
jiho kim <sokojiho2@gmail.com>
2022-03-07 17:27:40 +0900

    elice codding study

---
+++ Elice_if.js
@@ -0,0 +1,15 @@
+var a =20;
+var b =40;
+var c = 60;
+if (a>b){
+    console.log("a는 b보다 크다")
+} else{
+    console.log("a는 b보다 작거나 같다")
+}
+
+if (a>b){
+    console.log("a는 b보다 크다")
+} else if(b>c){
+    console.log("a는 b보다 작거나 같다")
+}
+console.log()
