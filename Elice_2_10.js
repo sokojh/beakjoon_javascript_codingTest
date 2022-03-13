f63fa0103bb311f84d85df4989b5a5242019865b
jiho kim <sokojiho2@gmail.com>
2022-03-13 19:12:11 +0900

    Elice edit

--- Elice_2_10.js
+++ Elice_2_10.js
@@ -19,24 +19,28 @@ rl.on("line", function (x) {
     }
 
 }).on("close", function () {
-    let ts = parseInt(input[0]);
-    let tu = parseInt(input[1]);
-    let result = tu/ts;
-    const temp1 = result * 1000
-    const temp2 = Math.floor(temp1)
-    let str = temp2+""
-    let str2 ="";
-    for(let i=0;i<str.length;i++){
-        if(str[i]!="0"){
-            if(i==0){
-                str2+=str[i]+"할 "
-            }else if(i==1){
-                str2+=str[i]+"푼 "
-            }else if(i==2){
-                str2+=str[i]+"리 "
+    if(parseInt(input[0])+parseInt(input[1])===0 || isNaN(parseInt(input[0]))){
+
+    }else {
+        let ts = parseInt(input[0])+16;
+        let tu = parseInt(input[1])+6;
+        let result = tu / ts;
+        const temp1 = result * 1000
+        const temp2 = Math.floor(temp1)
+        let str = temp2 + ""
+        let str2 = "";
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] != "0") {
+                if (i == 0) {
+                    str2 += str[i] + "할 "
+                } else if (i == 1) {
+                    str2 += str[i] + "푼 "
+                } else if (i == 2) {
+                    str2 += str[i] + "리 "
+                }
             }
         }
+        let str3 = str2.split(" ")
+        str3.forEach(element => console.log(element));
     }
-    let str3=str2.split(" ")
-    str3.forEach(element => console.log(element));
 });
