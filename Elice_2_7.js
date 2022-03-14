de31831eee04875d6471bf364161b20dbcad5c61
jiho kim <sokojiho2@gmail.com>
2022-03-14 14:59:12 +0900

    fibonacci 수열문제

--- Elice_2_7.js
+++ Elice_2_7.js
@@ -1,6 +1,23 @@
 const readline = require("readline");
 let input = "";
 let result = [];
+function fibonacci(n) {
+    //일단 초기 배열이 [0, 1]에서 시작하여 배열의 요소를 누적해 나가는 방법
+    //그리고 이미 구해놓은 것은 배열의 요소로 저장해놓기..!!! 그래야 런타임이 초과되지 않는다
+
+    let newArr = [0, 1]; //0번째 1번째 요소는 고정시켜두고
+
+    let fib = (n) => { //함수 한개를 선언해주고
+        if (newArr[n] !== undefined){
+            return newArr[n]; //이미 있는 건 그대로 리턴
+        }
+          newArr[n] = fib(n - 1) + fib(n - 2); //없는 건 새로 만들어서 저장!!!*****
+        return newArr[n];
+
+    };
+
+    return fib(n),newArr;
+}
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -10,7 +27,15 @@ rl.on("line", function (line) {
     input = line
     rl.close();
 }).on("close", function () {
-
+    let result = fibonacci(parseInt(input))
+    let resultArr = [];
+    for(let i=0;i<result.length;i++){
+        if(result[i]<parseInt(input)) {
+            resultArr.push(result[i]);
+        }
+    }
+    console.log(resultArr)
+/**
     result.push(0,1)
 
     for(let i=1;i<parseInt(input);i++){
@@ -23,5 +48,5 @@ rl.on("line", function (line) {
         result.pop();
     }
     console.log(result)
-
+**/
 });
