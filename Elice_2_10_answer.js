0f83c39b722ecf6bcc5d2453017bfefa5b1716f1
jiho kim <sokojiho2@gmail.com>
2022-04-13 17:54:28 +0900

    thth

--- Elice_2_10_answer.js
+++ Elice_2_10_answer.js
@@ -1,23 +1,18 @@
-input = [["100","<","300"]]
+var inputA = [{
+    name: "박태환",
+    age: 35
+},
+    {
+        name: "유재석",
+        age: 49
+    },
+    {
+        name: "김강훈",
+        age: 12
+    },
+    {
+        name: "이지원",
+        age: 15
+    }]
 
-let num1 = parseInt(input[0][0]);
-let num2 = parseInt(input[0][2]);
-let n = input[0][1];
-
-if(n=="<"){
-    if(num1<num2){
-        console.log(true);
-    }else{console.log(false);
-}}else if(n==">"){
-    if(num1>num2){
-        console.log(true);
-
-    }else {
-        console.log(false);
-    }}else if(n=="=="){
-        if(num1===num2){
-            console.log(true);
-        }else {
-            console.log(false);
-        }
-}
+console.log(inputA);
