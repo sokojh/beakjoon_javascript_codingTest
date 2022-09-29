bc2c9988bf7ee05a1b89e48d7da2acb8d9a71a33
jiho kim <sokojiho2@gmail.com>
2022-09-29 22:36:41 +0900

    feat: num await

--- Elice_javascriptPratice/await_.js
+++ Elice_javascriptPratice/await_.js
@@ -1,10 +1,14 @@
 const pratice = async ()=> {
 
+    let num = 0;
     await console.log(1);
     console.log(2);
     console.log(3);
     await console.log(4);
     await console.log(1)
+    num+1
+    num+2
+    console.log(num)
 
 }
 pratice();
